c9ed375bb7530cf4d3ee0eac2bbf6ba0b286f614
Gulab <gulabdhapa2001@gmail.com>
2026-05-03 15:34:50 +0530

    feat: add zombie and skeleton body part items with registry and asset support

--- common/src/main/java/net/gbdhapa/item/ModItems.java
+++ common/src/main/java/net/gbdhapa/item/ModItems.java
@@ -1,18 +1,27 @@
 package net.gbdhapa.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
+import net.gbdhapa.Mobparts;
 
 public class ModItems {
-    // Items moved to loader-specific registry for troubleshooting
-    public static Item ZOMBIE_HAND;
-    public static Item ZOMBIE_FOOT;
-    public static Item ZOMBIE_LEG;
-    public static Item ZOMBIE_TORSO;
-    public static Item SKELETON_HAND;
-    public static Item SKELETON_FOOT;
-    public static Item SKELETON_LEG;
-    public static Item SKELETON_TORSO;
+    public static final Item ZOMBIE_HAND = register("zombie_hand");
+    public static final Item ZOMBIE_FOOT = register("zombie_foot");
+    public static final Item ZOMBIE_LEG = register("zombie_leg");
+    public static final Item ZOMBIE_TORSO = register("zombie_torso");
+    
+    public static final Item SKELETON_HAND = register("skeleton_hand");
+    public static final Item SKELETON_FOOT = register("skeleton_foot");
+    public static final Item SKELETON_LEG = register("skeleton_leg");
+    public static final Item SKELETON_TORSO = register("skeleton_torso");
+
+    private static Item register(String name) {
+        ResourceKey<Item> key = ResourceKey.create(Registries.ITEM, Mobparts.id(name));
+        return new Item(new Item.Properties().setId(key));
+    }
 
     public static void init() {
+        // Just to trigger class loading
     }
 }
--- fabric/src/main/java/net/gbdhapa/fabric/registry/ModRegistry.java
+++ fabric/src/main/java/net/gbdhapa/fabric/registry/ModRegistry.java
@@ -11,33 +11,34 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.gbdhapa.Mobparts;
+import net.gbdhapa.item.ModItems;
 import net.gbdhapa.block.ModBlocks;
 
 public class ModRegistry {
 
-    public static final Item ZOMBIE_HAND = registerItemField("zombie_hand");
-    public static final Item ZOMBIE_FOOT = registerItemField("zombie_foot");
-    public static final Item ZOMBIE_LEG = registerItemField("zombie_leg");
-    public static final Item ZOMBIE_TORSO = registerItemField("zombie_torso");
-    
-    public static final Item SKELETON_HAND = registerItemField("skeleton_hand");
-    public static final Item SKELETON_FOOT = registerItemField("skeleton_foot");
-    public static final Item SKELETON_LEG = registerItemField("skeleton_leg");
-    public static final Item SKELETON_TORSO = registerItemField("skeleton_torso");
-
     public static void register() {
+        registerItem("zombie_hand", ModItems.ZOMBIE_HAND);
+        registerItem("zombie_foot", ModItems.ZOMBIE_FOOT);
+        registerItem("zombie_leg", ModItems.ZOMBIE_LEG);
+        registerItem("zombie_torso", ModItems.ZOMBIE_TORSO);
+        
+        registerItem("skeleton_hand", ModItems.SKELETON_HAND);
+        registerItem("skeleton_foot", ModItems.SKELETON_FOOT);
+        registerItem("skeleton_leg", ModItems.SKELETON_LEG);
+        registerItem("skeleton_torso", ModItems.SKELETON_TORSO);
+
         registerBlock("statue_pedestal", ModBlocks.STATUE_PEDESTAL);
 
         // Creative Tab Integration for 26.1 (Tiny Takeover)
         CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS).register(output -> {
-            output.accept(ZOMBIE_HAND);
-            output.accept(ZOMBIE_FOOT);
-            output.accept(ZOMBIE_LEG);
-            output.accept(ZOMBIE_TORSO);
-            output.accept(SKELETON_HAND);
-            output.accept(SKELETON_FOOT);
-            output.accept(SKELETON_LEG);
-            output.accept(SKELETON_TORSO);
+            output.accept(ModItems.ZOMBIE_HAND);
+            output.accept(ModItems.ZOMBIE_FOOT);
+            output.accept(ModItems.ZOMBIE_LEG);
+            output.accept(ModItems.ZOMBIE_TORSO);
+            output.accept(ModItems.SKELETON_HAND);
+            output.accept(ModItems.SKELETON_FOOT);
+            output.accept(ModItems.SKELETON_LEG);
+            output.accept(ModItems.SKELETON_TORSO);
         });
 
         CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(output -> {
@@ -45,11 +46,8 @@ public class ModRegistry {
         });
     }
 
-    private static Item registerItemField(String name) {
-        Identifier id = Mobparts.id(name);
-        ResourceKey<Item> key = ResourceKey.create(Registries.ITEM, id);
-        Item item = new Item(new Item.Properties().setId(key));
-        return Registry.register(BuiltInRegistries.ITEM, id, item);
+    private static void registerItem(String name, Item item) {
+        Registry.register(BuiltInRegistries.ITEM, Mobparts.id(name), item);
     }
 
     private static void registerBlock(String name, Block block) {
